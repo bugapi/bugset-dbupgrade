61009c6abb0967dba2619ff25f1b29da7a9b82ac
gust <gust@citydo.com.cn>
2019-10-31 17:32:48 +0800

    目录结构调整

--- src/main/java/org/bugapi/bugset/component/dbupgrade/executer/DatabaseUpgradeExecutor.java
+++ src/main/java/org/bugapi/bugset/component/dbupgrade/executer/DatabaseUpgradeExecutor.java
@@ -1,25 +1,24 @@
 package org.bugapi.bugset.component.dbupgrade.executer;
 
 import javax.sql.DataSource;
+import lombok.extern.slf4j.Slf4j;
 import org.bugapi.bugset.base.constant.EnvironmentEnum;
 import org.bugapi.bugset.component.dbupgrade.parse.BaseUpgradeConfigParse;
 import org.bugapi.bugset.component.dbupgrade.upgrade.DefaultUpgrade;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
- * @ClassName: UpgradeProjectExecuter
- * @Description: 项目升级执行器
- * @author: zhangxw
- * @date: 2019/5/4
+ * 项目升级执行器
+ *
+ * @author gust
+ * @since 0.0.1
  */
-public class UpgradeProjectExecuter {
-	private static Logger logger = LoggerFactory.getLogger(UpgradeProjectExecuter.class);
+@Slf4j
+public class DatabaseUpgradeExecutor {
 
 	private BaseUpgradeConfigParse baseUpgradeConfigParse;
 	private DataSource dataSource;
 
-	public UpgradeProjectExecuter(BaseUpgradeConfigParse baseUpgradeConfigParse, DataSource dataSource) {
+	public DatabaseUpgradeExecutor(BaseUpgradeConfigParse baseUpgradeConfigParse, DataSource dataSource) {
 		this.baseUpgradeConfigParse = baseUpgradeConfigParse;
 		this.dataSource = dataSource;
 	}
@@ -33,15 +32,15 @@ public class UpgradeProjectExecuter {
 	public void execute(EnvironmentEnum environmentType, Boolean isAutoUpgrade) {
 		try {
 			if(!isAutoUpgrade){
-				logger.error("没有启动自动化升级");
+				log.error("没有启动自动化升级");
 				return;
 			}
-			logger.error("开始升级数据库...");
+			log.error("开始升级数据库...");
 			DefaultUpgrade defaultUpgrade = new DefaultUpgrade(baseUpgradeConfigParse);
 			defaultUpgrade.upgrade(dataSource, environmentType);
-			logger.error("数据库升级完成...");
+			log.error("数据库升级完成...");
 		} catch (Exception e) {
-			logger.error("数据库升级失败...");
+			log.error("数据库升级失败，失败原因：{}", e.getMessage());
 		}
 	}
 }
